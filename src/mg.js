ae0ef1593496ea066fb206e359aa4e9681bf7ffb
Nat <nat@cmmc.io>
2017-02-14 14:57:11 +0700

    Simplify

--- src/mg.js
+++ src/mg.js
@@ -16,6 +16,9 @@ const MGREV = 'NJS1a';
 
 let appid, appkey, appsecret = ""
 
+const gearauthurl = 'http://' + GEARAPIADDRESS + ':' + GEARAPIPORT;
+let verifier = MGREV;
+
 
 export class NetpieOAuth {
   constructor (props) {
@@ -55,81 +58,62 @@ export class NetpieOAuth {
     return out;
   }
 
-  async request (url, data) {
-    let ret = fetch(url, {
+  async request (data, auth_func) {
+    let ret = fetch(data.url, {
       method: data.method,
       headers: {
-        'Authorization': this.oauth.toHeader(this.oauth.authorize(data)).Authorization,
+        'Authorization': auth_func.apply(this, [data]),
       }
     });
 
     return ret;
   }
 
-  OAuthGetRequestToken = async () => {
-    console.log("OAuthGetRequest");
-    let requesttoken = {};
-    let gearauthurl = 'http://' + GEARAPIADDRESS + ':' + GEARAPIPORT;
-    let verifier = MGREV;
-
-    console.log("GearAuthURL", gearauthurl);
-
-    let request_data = {
-      url: gearauthurl + '/api/rtoken',
-      method: 'POST',
-      data: {
-        oauth_callback: 'scope=&appid=' + appid + '&mgrev=' + MGREV + '&verifier=' + verifier,
-      }
+  build_request_object (uri, method = 'POST') {
+    let obj = {
+      method: method,
+      url: gearauthurl + uri,
     };
 
-    let resp = await this.request(request_data.url, request_data)
-    let text1 = await resp.text();
-    console.log('text1', text1)
+    let ret = {
+      object: () => obj,
+      data: (val) => {
+        obj.data = val
+        return ret;
+      }
+    }
 
-    // try {
-    //   console.log("REQ 1")
-    //   let req1 = await fetch(request_data.url, {
-    //     method: request_data.method,
-    //     headers: {
-    //       'Authorization': this.oauth.toHeader(this.oauth.authorize(request_data)).Authorization,
-    //     }
-    //   });
+    return ret;
+  }
+
+  OAuthGetRequestToken = async () => {
+    let request_data = this.build_request_object('/api/rtoken')
+    .data({oauth_callback: 'scope=&appid=' + appid + '&mgrev=' + MGREV + '&verifier=' + verifier});
+
+    // request token
+    let resp = await this.request(request_data.object(), (data) => {
+      return this.oauth.toHeader(this.oauth.authorize(data)).Authorization
+    })
+
+    let {oauth_token, oauth_token_secret, oauth_callback_confirmed} = this.extract(await resp.text());
     //
-    //   console.log("WAIT REQ 1")
-    //   let body = await req1.text();
-    //   console.log("/WAIT REQ 1")
-    //   console.log("BODY >>>>>", body)
-    //   let response_extracted = this.extract(body);
-    //   requesttoken.token = response_extracted.oauth_token;
-    //   requesttoken.secret = response_extracted.oauth_token_secret;
-    //   requesttoken.verifier = verifier;
-    //
-    //   let reqtok = {
-    //     key: requesttoken.token,
-    //     secret: requesttoken.secret
-    //   };
-    //
-    //   // access token
-    //   let request_access_token = {
-    //     url: gearauthurl + '/api/atoken',
-    //     method: 'POST',
-    //     data: {
-    //       oauth_verifier: verifier,
-    //     }
-    //   };
-    //
-    //   let req3 = await fetch(request_access_token.url, {
-    //     method: request_access_token.method,
-    //     headers: {
-    //       'Authorization': this.oauth.toHeader(this.oauth.authorize(request_access_token, reqtok)).Authorization
-    //     }
-    //   });
-    //
-    //   return req3.text();
-    // }
-    // catch (error) {
-    //   console.log("error: ", error);
-    // }
+    // console.log('oauth_token', oauth_token)
+    // console.log('oauth_token_secret', oauth_token_secret)
+    // console.log('oauth_token_callback_confirmed', oauth_callback_confirmed)
+
+    let request_data2 = this.build_request_object('/api/atoken').data({oauth_verifier: verifier})
+
+    // for verification
+    let reqtok = {
+      key: oauth_token,
+      secret: oauth_token_secret
+    };
+
+    let resp2 = await this.request(request_data2.object(), (req_acc_token) => {
+      return this.oauth.toHeader(this.oauth.authorize(req_acc_token, reqtok)).Authorization
+    })
+
+    return this.extract(await resp2.text())
   };
 }
 
