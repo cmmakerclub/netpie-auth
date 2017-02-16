c7f6249cd05e23e3853235d6dfd69ada7ca756e2
Nat <nat@cmmc.io>
2017-02-16 16:17:24 +0700

    Add helper function

--- dist/run.js
+++ dist/run.js
@@ -13,14 +13,21 @@ var appsecret = "LSOMysLxZSKh6CYVarL1IDPkK";
 
 var netpie = new _mg.NetpieAuth({ appid: appid, appkey: appkey, appsecret: appsecret });
 
+var hmac = function hmac(text, key) {
+  return base64(CryptoJS.HmacSHA1(text, key));
+};
+var base64 = function base64(text) {
+  return text.toString(CryptoJS.enc.Base64);
+};
+
 var compute_hkey = function compute_hkey(access_token_secret, app_secret) {
   return access_token_secret + '&' + app_secret;
 };
 var compute_mqtt_password = function compute_mqtt_password(access_token, mqttusername, hkey) {
-  return CryptoJS.HmacSHA1(access_token + '%' + mqttusername, hkey).toString(CryptoJS.enc.Base64);
+  return hmac(access_token + '%' + mqttusername, hkey);
 };
 var compute_revoke_code = function compute_revoke_code(access_token, hkey) {
-  return CryptoJS.HmacSHA1(access_token, hkey).toString(CryptoJS.enc.Base64).replace(/\//g, '_');
+  return hmac(access_token, hkey).replace(/\//g, '_');
 };
 
 netpie.getToken().then(function (token) {
@@ -29,9 +36,11 @@ netpie.getToken().then(function (token) {
       endpoint = token.endpoint,
       flag = token.flag;
 
+
   var hkey = compute_hkey(oauth_token_secret, appsecret);
   var mqttusername = appkey + '%' + Math.floor(Date.now() / 1000);
   var mqttpassword = compute_mqtt_password(oauth_token, mqttusername, hkey);
+
   var revoke_code = compute_revoke_code(oauth_token, hkey);
 
   var command_t = 'mosquitto_sub -t "/' + appid + '/gearname/#" -h gb.netpie.io -i ' + oauth_token + ' -u "' + mqttusername + '" -P "' + mqttpassword + '" -d';
--- src/run.js
+++ src/run.js
@@ -13,17 +13,21 @@ const appsecret = "LSOMysLxZSKh6CYVarL1IDPkK";
 
 let netpie = new NetpieAuth({appid: appid, appkey: appkey, appsecret: appsecret});
 
+
+let hmac = (text, key) => base64(CryptoJS.HmacSHA1(text, key))
+let base64 = (text) => text.toString(CryptoJS.enc.Base64)
+
 let compute_hkey = (access_token_secret, app_secret) => `${access_token_secret}&${app_secret}`
-let compute_mqtt_password = (access_token, mqttusername, hkey) =>
-  CryptoJS.HmacSHA1(`${access_token}%${mqttusername}`, hkey).toString(CryptoJS.enc.Base64);
-let compute_revoke_code = (access_token, hkey) =>
-  CryptoJS.HmacSHA1(access_token, hkey).toString(CryptoJS.enc.Base64).replace(/\//g, '_');
+let compute_mqtt_password = (access_token, mqttusername, hkey) => hmac(`${access_token}%${mqttusername}`, hkey)
+let compute_revoke_code = (access_token, hkey) => hmac(access_token, hkey).replace(/\//g, '_');
 
 netpie.getToken().then((token) => {
   let {oauth_token, oauth_token_secret, endpoint, flag} = token
+
   let hkey = compute_hkey(oauth_token_secret, appsecret)
   let mqttusername = `${appkey}%${Math.floor(Date.now() / 1000)}`;
   let mqttpassword = compute_mqtt_password(oauth_token, mqttusername, hkey)
+
   let revoke_code = compute_revoke_code(oauth_token, hkey)
 
   let command_t = `mosquitto_sub -t "/${appid}/gearname/#" -h gb.netpie.io -i ${oauth_token} -u "${mqttusername}" -P "${mqttpassword}" -d`;
